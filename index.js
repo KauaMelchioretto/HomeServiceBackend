003edb3942bf3812661a9874bfada0581e124ea5
Kauã Melchioretto <kauamelchoretto@gmail.com>
2023-04-18 23:38:58 -0300

    Adjusts to deploy server again

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql2");
+const postGree = require("pg");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const SECRET = "password";
 
-const dataBase = mysql.createPool({
-  host: "us-cdbr-east-05.cleardb.net",
-  user: "bf11e650684986",
-  password: "ea084074",
-  database: "heroku_3bb06900a2cf28a",
+const dataBase = new postGree.Pool({
+  connectionString: process.env.CONNECTION_STRING
 });
+
+dataBase.connect((err) => {
+  if (err) console.log(err);
+  else console.log("connected succesfully"), createDataBaseTables();
+});
+
+async function createDataBaseTables() {
+  let usersTable =
+    "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY NOT NULL UNIQUE, username varchar(60) NOT NULL, email varchar(45) NOT NULL UNIQUE, password varchar(45) NOT NULL, active BOOLEAN NOT NULL);";
+
+  let servicesTable =
+    "CREATE TABLE IF NOT EXISTS services (id SERIAL PRIMARY KEY NOT NULL UNIQUE, user_id SERIAL NOT NULL, name varchar(45) NOT NULL, profession varchar(45) NOT NULL, city varchar(45) NOT NULL, cit2 varchar(45) DEFAULT NULL, phone_number varchar(45) NOT NULL, description varchar(100) DEFAULT NULL, CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (id));";
+
+  let avaliationsTable =
+    "CREATE TABLE IF NOT EXISTS avaliations (id SERIAL PRIMARY KEY NOT NULL UNIQUE, service_id int NOT NULL, username varchar(60) NOT NULL, comment varchar(350) DEFAULT NULL, avaliation int NOT NULL,CONSTRAINT service_id FOREIGN KEY (service_id) REFERENCES services (id))";
+
+  var queries = [];
+  queries.push(usersTable, servicesTable, avaliationsTable);
+
+  for (let i = 0; i < queries.length; i++) {
+    try {
+      let res = await dataBase.query(queries[i]);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`rodando server na porta ${process.env.PORT}`);
 });
 
-app.use(cors({ credentials:true, origin:"https://webhomeservice.herokuapp.com" }));
+//app.use(cors({ credentials:true, origin:"http://localhost:3000" }));
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
 
 function verifyJWT(request, response) {
   const token = jwt.decode(request);
@@ -27,7 +52,6 @@ function verifyJWT(request, response) {
   if (idUser != undefined) return idUser;
   else return response.status(401).end();
 }
-
 
 app.post("/registrosDeServicos", (request, response) => {
   const { userToken } = request.body;
@@ -65,6 +89,7 @@ app.post("/getCards", (request, response) => {
 
 app.post("/resultados", (request, response) => {
   const { information } = request.body;
+  console.log(information);
   if (information != null && information != " " && information != "") {
     SQL = `SELECT idservice , name, profession, city, city2, numberTel, description FROM services WHERE LOCATE (?, name) > 0 OR LOCATE (?, profession) > 0 OR LOCATE (?, city) > 0 OR LOCATE (?, city2) > 0 `;
 
@@ -81,11 +106,11 @@ app.post("/resultados", (request, response) => {
   }
 });
 
-function getUserNameJWT(request,response) {
+function getUserNameJWT(request, response) {
   const token = jwt.decode(request);
   var username = JSON.stringify(token.result.map((value) => value.username));
-  username = username.replace(/[[\]\\"]/g, ''); 
-  if(username != undefined) return username;
+  username = username.replace(/[[\]\\"]/g, "");
+  if (username != undefined) return username;
   else return response.status(401).end();
 }
 
@@ -142,10 +167,11 @@ app.post("/registroUsuario", (request, response) => {
   const { email } = request.body;
   const { password } = request.body;
 
-  let SQL = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-  dataBase.query(SQL, [userName, email, password], (err, result) => {
+  let SQL =
+    "INSERT INTO users (username, email, password, active) VALUES (?, ?, ?, ?)";
+  dataBase.query(SQL, [userName, email, password, false], (err, result) => {
     if (err) console.log(err);
-    else response.send(result);
+    else response.sendStatus(200);
   });
 });
 
@@ -154,18 +180,19 @@ app.post("/login", (request, response) => {
   const { password } = request.body;
   let token;
 
-  let SQL = "SELECT iduser, username FROM users WHERE ? = email AND ? = password";
+  let SQL =
+    "SELECT iduser, username FROM users WHERE ? = email AND ? = password";
   dataBase.query(SQL, [email, password], (err, result) => {
     if (err) console.log(err);
     else {
       if (result.length !== 0) {
-        token = jwt.sign({ result }, SECRET, { expiresIn: "1h" });
+        token = jwt.sign({ result }, process.env.SECRET, { expiresIn: "1h" });
         response.cookie("token", token, {
           path: "/",
           expires: new Date(Date.now() + 1200000),
           httpOnly: false,
           secure: true,
-          sameSite: "none"
+          sameSite: "none",
         });
         response.json({ auth: true, token, result });
       } else response.send(null);
@@ -173,12 +200,12 @@ app.post("/login", (request, response) => {
   });
 });
 
-app.get("/getcookie", function (request, response){
+app.get("/getcookie", function (request, response) {
   response.send(request.cookies);
 });
 
-app.get('/clearcookie', function(req, res) {
-  res.clearCookie('token', {path: '/', secure: true, sameSite: "none"});
+app.get("/clearcookie", function (req, res) {
+  res.clearCookie("token", { path: "/", secure: true, sameSite: "none" });
   res.end();
 });
 
