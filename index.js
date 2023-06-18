0eaa4ed96401ec039c54682b74b0aed80d0086da
Kauã Melchioretto <kauamelchoretto@gmail.com>
2023-06-18 16:23:33 -0300

    remove domain config from set cookies

--- index.js
+++ index.js
@@ -197,7 +197,6 @@ app.post("/login", (request, response) => {
         response.cookie("token", token, {
           path: "/",
           expires: new Date(Date.now() + 1200000),
-          domain: 'https://homeservice-ute7.onrender.com/',
           httpOnly: false,
           secure: true,
           sameSite: "none",
