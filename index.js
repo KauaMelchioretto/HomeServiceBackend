454a55b4ed2cd77bd0c1a6474007e0a0a39552f2
Kauã Melchioretto <kauamelchoretto@gmail.com>
2023-07-07 16:25:47 -0300

    remove cookie mechanic to implement local storage

--- index.js
+++ index.js
@@ -3,16 +3,10 @@ const app = express();
 const postGree = require("pg");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
 require('dotenv').config();
 
 const dataBase = new postGree.Pool({
   connectionString: process.env.CONNECTION_STRING
-  // host: process.env.DB_HOST,
-  // port: process.env.DB_PORT,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_DATABASE
 });
 
 app.listen(process.env.PORT || 3001, () => {
@@ -21,7 +15,6 @@ app.listen(process.env.PORT || 3001, () => {
 
 app.use(cors({ credentials:true, origin:"https://homeservice-ute7.onrender.com" }));
 app.use(express.json());
-app.use(cookieParser());
 
 dataBase.connect((err) => {
   if (err) console.log(err);
@@ -33,7 +26,7 @@ async function createDataBaseTables() {
     "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY NOT NULL UNIQUE, username varchar(60) NOT NULL, email varchar(45) NOT NULL UNIQUE, password varchar(45) NOT NULL, active BOOLEAN NOT NULL);";
 
   let servicesTable =
-    "CREATE TABLE IF NOT EXISTS services (id SERIAL PRIMARY KEY NOT NULL UNIQUE, user_id SERIAL NOT NULL, name varchar(45) NOT NULL, profession varchar(45) NOT NULL, city varchar(45) NOT NULL, cit2 varchar(45) DEFAULT NULL, phone_number varchar(45) NOT NULL, description varchar(100) DEFAULT NULL, CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (id));";
+    "CREATE TABLE IF NOT EXISTS services (id SERIAL PRIMARY KEY NOT NULL UNIQUE, user_id SERIAL NOT NULL, name varchar(45) NOT NULL, profession varchar(45) NOT NULL, city varchar(45) NOT NULL, city2 varchar(45) DEFAULT NULL, phone_number varchar(45) NOT NULL, description varchar(100) DEFAULT NULL, CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (id));";
 
   let avaliationsTable =
     "CREATE TABLE IF NOT EXISTS avaliations (id SERIAL PRIMARY KEY NOT NULL UNIQUE, service_id int NOT NULL, username varchar(60) NOT NULL, comment varchar(350) DEFAULT NULL, avaliation int NOT NULL,CONSTRAINT service_id FOREIGN KEY (service_id) REFERENCES services (id))";
@@ -55,26 +48,26 @@ async function createDataBaseTables() {
 
 function verifyJWT(request, response) {
   const token = jwt.decode(request);
-  const idUser = parseInt(token.result.map((value) => value.iduser));
-  if (idUser != undefined) return idUser;
+  const userId = parseInt(token.result.rows.map((value) => value.id));
+  if (userId != undefined) return userId;
   else return response.status(401).end();
 }
 
 app.post("/registrosDeServicos", (request, response) => {
   const { userToken } = request.body;
-  const iduser = verifyJWT(userToken);
+  const userId = verifyJWT(userToken);
   const { name } = request.body;
   const { profession } = request.body;
   const { city } = request.body;
   const { city2 } = request.body;
-  const { numberTel } = request.body;
+  const { phoneNumber } = request.body;
   const { description } = request.body;
 
-  let SQL = `INSERT INTO services (iduser, name, profession, city, city2, numberTel, description) VALUES ( $1, $2, $3, $4, $5, $6, $7)`;
+  let SQL = `INSERT INTO services (user_id, name, profession, city, city2, phone_number, description) VALUES ( $1, $2, $3, $4, $5, $6, $7)`;
 
   dataBase.query(
     SQL,
-    [iduser, name, profession, city, city2, numberTel, description],
+    [userId, name, profession, city, city2, phoneNumber, description],
     (err, result) => {
       if (err) console.log(err);
       else response.send(result);
@@ -84,28 +77,28 @@ app.post("/registrosDeServicos", (request, response) => {
 
 app.post("/getCards", (request, response) => {
   const { userToken } = request.body;
-  const iduser = verifyJWT(userToken);
+  const userId = verifyJWT(userToken);
 
-  let SQL = "SELECT * FROM services WHERE $1 = iduser";
+  let SQL = "SELECT * FROM services WHERE $1 = user_id";
 
-  dataBase.query(SQL, [iduser], (err, result) => {
+  dataBase.query(SQL, [userId], (err, result) => {
+    console.log(JSON.stringify(result));
     if (err) console.log(err);
-    else response.send(result);
+    else response.send(result.rows);
   });
 });
 
 app.post("/resultados", (request, response) => {
   const { information } = request.body;
-  console.log(information);
   if (information != null && information != " " && information != "") {
-    SQL = `SELECT idservice , name, profession, city, city2, numberTel, description FROM services WHERE LOCATE ($1, name) > 0 OR LOCATE ($2, profession) > 0 OR LOCATE ($3, city) > 0 OR LOCATE ($4, city2) > 0 `;
+    SQL = `SELECT id , name, profession, city, city2, phone_number, description FROM services WHERE name ~* $1 OR profession ~* $1 OR city ~* $1 OR city2 ~* $1`;
 
     dataBase.query(
       SQL,
-      [information, information, information, information],
+      [information],
       (err, result) => {
         if (err) console.log(err);
-        else response.send(result);
+        else response.send(result.rows);
       }
     );
   } else {
@@ -113,9 +106,9 @@ app.post("/resultados", (request, response) => {
   }
 });
 
-function getUserNameJWT(request, response) {
+function _getUserNameJWT(request, response) {
   const token = jwt.decode(request);
-  var username = JSON.stringify(token.result.map((value) => value.username));
+  var username = JSON.stringify(token.result.rows.map((value) => value.username));
   username = username.replace(/[[\]\\"]/g, "");
   if (username != undefined) return username;
   else return response.status(401).end();
@@ -123,23 +116,23 @@ function getUserNameJWT(request, response) {
 
 app.post("/getUserName", (request, response) => {
   const { userToken } = request.body;
-  const username = getUserNameJWT(userToken);
-  response.send(username);
+    const userName = _getUserNameJWT(userToken);
+    response.send(userName);
 });
 
 app.post("/registrarAvaliacao", (request, response) => {
-  const { idService } = request.body;
+  const { serviceId } = request.body;
   const { userToken } = request.body;
-  const username = getUserNameJWT(userToken);
+  const username = _getUserNameJWT(userToken);
   const { comment } = request.body;
   const { avaliation } = request.body;
 
   let SQL =
-    "INSERT INTO avaliations (idservice, username, comment, avaliation) VALUES ($1, $2, $3, $4)";
+    "INSERT INTO avaliations (service_id, username, comment, avaliation) VALUES ($1, $2, $3, $4)";
 
   dataBase.query(
     SQL,
-    [idService, username, comment, avaliation],
+    [serviceId, username, comment, avaliation],
     (err, result) => {
       if (err) console.log(err);
       else response.send(result);
@@ -148,14 +141,14 @@ app.post("/registrarAvaliacao", (request, response) => {
 });
 
 app.post("/getAvaliations", (request, response) => {
-  const { idService } = request.body;
+  const { serviceId } = request.body;
 
   let SQL =
-    "SELECT idavaliation, username, comment, avaliation FROM avaliations WHERE $1 = idservice";
+    "SELECT id, username, comment, avaliation FROM avaliations WHERE $1 = service_id";
 
-  dataBase.query(SQL, [idService], (err, result) => {
+  dataBase.query(SQL, [serviceId], (err, result) => {
     if (err) console.log(err);
-    else response.send(result);
+    else response.send(result.rows);
   });
 });
 
@@ -194,46 +187,31 @@ app.post("/login", (request, response) => {
     else {
       if (result.rows.length !== 0) {
         token = jwt.sign({ result }, process.env.SECRET, { expiresIn: "1h" });
-        response.cookie("token", token, {
-          path: "/",
-          expires: new Date(Date.now() + 1200000),
-          httpOnly: false,
-          secure: true,
-          sameSite: "none",
-        });
         response.json({ auth: true, token, result });
       } else response.send(null);
     }
   });
 });
 
-app.get("/getcookie", function (request, response) {
-  response.send(request.cookies);
-});
-
-app.get("/clearcookie", function (req, res) {
-  res.clearCookie("token", { path: "/", secure: true, sameSite: "none" });
-  res.end();
-});
-
 app.put("/editService", (request, response) => {
+  console.log(`request: ${JSON.stringify(request.body)}`)
   const { id } = request.body;
   const { name } = request.body;
   const { profession } = request.body;
-  const city = request.body.city;
-  const city2 = request.body.city2;
-  const numberTel = request.body.numberTel;
-  const description = request.body.description;
+  const { city } = request.body;
+  const { city2 } = request.body;
+  const { phoneNumber } = request.body;
+  const { description } = request.body;
 
   let SQL =
-    "UPDATE services SET name = $1, profession = $2, city = $3, city2 = $4, numberTel = $5, description = $6 WHERE idservice = $7";
+    "UPDATE services SET name = $1, profession = $2, city = $3, city2 = $4, phone_number = $5, description = $6 WHERE id = $7";
 
   dataBase.query(
     SQL,
-    [name, profession, city, city2, numberTel, description, id],
+    [name, profession, city, city2, phoneNumber, description, id],
     (err, result) => {
       if (err) console.log(err);
-      else response.send(result);
+      else response.send(result.rows);
     }
   );
 });
@@ -241,7 +219,7 @@ app.put("/editService", (request, response) => {
 app.post("/deleteAvaliation", (request, response) => {
   const { id } = request.body;
 
-  let SQL = "DELETE FROM avaliations WHERE idservice = $1";
+  let SQL = "DELETE FROM avaliations WHERE service_id = $1";
   dataBase.query(SQL, [id], (err, result) => {
     if (err) console.log(err);
     else response.send(result);
@@ -251,7 +229,7 @@ app.post("/deleteAvaliation", (request, response) => {
 app.post("/deleteService", (request, response) => {
   const { id } = request.body;
 
-  let SQL = "DELETE FROM services WHERE idservice = $1";
+  let SQL = "DELETE FROM services WHERE id = $1";
   dataBase.query(SQL, [id], (err, result) => {
     if (err) console.log(err);
     else response.send(result);
